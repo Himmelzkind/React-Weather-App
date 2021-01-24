cffd9c617aa49778bce77ef5de1b50627da2be47
Himmelzkind <73137185+Himmelzkind@users.noreply.github.com>
2021-01-24 20:38:33 +0100

    try to correct error

--- src/WeatherToday.js
+++ src/WeatherToday.js
@@ -18,11 +18,11 @@ export default function WeatherToday() {
       />
       <strong id="currentTemp">{weatherData.temperature}</strong>
       <span className="units">
-        <a href="#" id="celsius-link" className="active">
+        <a href="" id="celsius-link" className="active">
           °C
         </a>
 
-        <a href="#" id="fahrenheit-link">
+        <a href="" id="fahrenheit-link">
           °F
         </a>
       </span>
